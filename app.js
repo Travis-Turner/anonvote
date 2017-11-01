462222adfa27a00f271e3b1ac8b5013afbece519
richardturnerdeveloper <richardturnerdeveloper@gmail.com>
2017-11-01 17:08:39 -0400

    Integrate mongoose and mongodb, save unencrypted user information

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+
+//mongoose setup
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/AnonVote");
+
+var userSchema = new mongoose.Schema({
+ email: String,
+ password: String
+});
+
+var User = mongoose.model("User", userSchema);
+
+//app config
 
 const PORT = 3000;
 
@@ -10,6 +27,12 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use("/public", express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(expressValidator());
+
 // GET ROUTES
 app.get("/", (req, res) => {
   res.render('home');
@@ -19,7 +42,14 @@ app.get("/register", (req, res) => {
 });
 // POST ROUTES
 app.post("/register", (req, res) => {
-  res.send('Registration placeholder!');
+  if (req.body.email && req.body.password){
+    var newUser = new User(req.body);
+    newUser.save().then(() => {
+      res.send(newUser);
+    });
+  } else {
+    res.send('Missing data!');
+  }
 });
 
 app.listen(PORT, () => {
